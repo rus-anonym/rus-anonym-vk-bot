f9e8187f0cacd7052b3760bb1a0f88de99f0a914
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-23 08:08:10 +0300

    add group message type

--- src/plugins/types.ts
+++ src/plugins/types.ts
@@ -1,6 +1,16 @@
 import { MessageContext, IMessageContextSendOptions } from "vk-io";
+import { IQuestionMessageContext } from "vk-io-question";
 
 export interface ModernUserMessageContext extends MessageContext {
+	sendMessage(
+		text: string | IMessageContextSendOptions,
+		params?: IMessageContextSendOptions | undefined,
+	): Promise<MessageContext<Record<string, any>>>;
+}
+
+export interface ModernGroupMessageContext
+	extends MessageContext,
+		IQuestionMessageContext {
 	sendMessage(
 		text: string | IMessageContextSendOptions,
 		params?: IMessageContextSendOptions | undefined,
