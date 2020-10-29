69668bdc9c37cb1491e482e1dc7d56865d52bfc9
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-29 22:34:30 +0300

    add loading commands to main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { userVK } from "./plugins/userVK";
 import { groupVK } from "./plugins/groupVK";
+import * as core from "./plugins/core";
 import utils from "rus-anonym-utils";
 
 (async function () {
 	await userVK.updates.start();
 	await groupVK.updates.start();
+	await core.loadCommands();
 	await utils.logger.console(`started`);
 })();
