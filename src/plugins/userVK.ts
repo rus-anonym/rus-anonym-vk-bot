ccc49ea5f9c6faadc246ca1e23dcb8a5c8ad9a2f
rus_anonym <alexandrsemin2033@gmail.com>
2020-10-29 22:30:50 +0300

    add command handler

--- src/plugins/userVK.ts
+++ src/plugins/userVK.ts
@@ -1,7 +1,7 @@
 import { groupLogger } from "./logger";
 import { config, userCommands } from "./core";
 import { ModernUserMessageContext, messageDataBase } from "./types";
-import { VK } from "vk-io";
+import { VK, IMessageContextSendOptions, MessageContext } from "vk-io";
 import utils from "rus-anonym-utils";
 import { DB } from "./db";
 
@@ -118,7 +118,45 @@ userVK.updates.use(async (message: ModernUserMessageContext) => {
 	}
 
 	if (message.isOutbox === true) {
-		return;
+		let command = userCommands.find((x) => x.regexp.test(message.text || ""));
+		if (!command) {
+			return;
+		}
+		message.sendMessage = async (
+			text: string | IMessageContextSendOptions,
+			params?: IMessageContextSendOptions | undefined,
+		): Promise<MessageContext<Record<string, any>>> => {
+			try {
+				let paramsForSend = Object.assign({ disable_mentions: true }, params);
+				return await message.send(
+					`@id${message.senderId}, ${text}`,
+					paramsForSend,
+				);
+			} catch (error) {
+				console.log(error);
+				return error;
+			}
+		};
+		if (message.text) {
+			message.args = message.text.match(command.regexp);
+		}
+		try {
+			await command.process(message);
+			return;
+		} catch (err) {
+			await message.sendMessage(`ошиб очка.`);
+			await message.send({
+				sticker_id: await utils.array.random([
+					18464,
+					16588,
+					18466,
+					18484,
+					14088,
+				]),
+			});
+			console.log(err);
+			return;
+		}
 	}
 
 	if (message.flags === 131200) {
@@ -159,40 +197,6 @@ userVK.updates.use(async (message: ModernUserMessageContext) => {
 			);
 		}
 	}
-
-	// let command = userCommands.find((x) => x.regexp.test(message.text || ""));
-	// if (!command) {
-	// 	return;
-	// }
-	// message.sendMessage = async (
-	// 	text: string | IMessageContextSendOptions,
-	// 	params?: IMessageContextSendOptions | undefined,
-	// ): Promise<MessageContext<Record<string, any>>> => {
-	// 	try {
-	// 		let paramsForSend = Object.assign({ disable_mentions: true }, params);
-	// 		return await message.send(
-	// 			`@id${message.senderId}, ${text}`,
-	// 			paramsForSend,
-	// 		);
-	// 	} catch (error) {
-	// 		console.log(error);
-	// 		return error;
-	// 	}
-	// };
-	// if (message.text) {
-	// 	message.args = message.text.match(command.regexp);
-	// }
-	// try {
-	// 	await command.process(message);
-	// 	return;
-	// } catch (err) {
-	// 	await message.sendMessage(`ошиб очка.`);
-	// 	await message.send({
-	// 		sticker_id: await utils.array.random([18464, 16588, 18466, 18484, 14088]),
-	// 	});
-	// 	console.log(err);
-	// 	return;
-	// }
 });
 
 export { userVK };
