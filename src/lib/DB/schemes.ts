0c69eb6582ffa48b8953f13d9f72ff718ad53c00
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-11 14:31:44 +0300

    create db schemes

---
+++ src/lib/DB/schemes.ts
@@ -0,0 +1,9 @@
+import { createSchema } from "ts-mongoose";
+
+const user = createSchema({});
+
+const chat = createSchema({});
+
+const message = createSchema({});
+
+export { user, chat, message };
