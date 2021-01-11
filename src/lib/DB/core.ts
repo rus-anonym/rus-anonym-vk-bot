7cf365184cee623ea72df75aed60c252a630d60e
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-11 14:24:04 +0300

    add database connect

--- src/lib/DB/core.ts
+++ src/lib/DB/core.ts
@@ -1,3 +1,25 @@
 import config from "../../DB/config.json";
+import utils from "rus-anonym-utils";
+import mongoose from "mongoose";
 
-export { config };
+export default {
+	connect: async function connectDataBase(): Promise<boolean> {
+		try {
+			await mongoose.connect(
+				`mongodb+srv://${config.mongoose.user}:${config.mongoose.password}@${config.mongoose.address}/${config.mongoose.database}?retryWrites=true&w=majority`,
+				{
+					useCreateIndex: true,
+					useNewUrlParser: true,
+					useUnifiedTopology: true,
+				},
+			);
+			utils.logger.info("Successful connect to database");
+			return true;
+		} catch (error) {
+			utils.logger.info("Connect to database");
+			return false;
+		}
+	},
+
+	config: config,
+};
--- src/lib/VK/core.ts
+++ src/lib/VK/core.ts
@@ -1,26 +1,26 @@
 import { VK } from "vk-io";
 import IVKInstance from "../../types/interfaces/vk";
-import { config } from "../DB/core";
+import DataBase from "../DB/core";
 
 const user: IVKInstance = {
-	id: config.vk.user.id,
-	sessions: config.vk.user.additional.map((tempUserToken) => {
+	id: DataBase.config.vk.user.id,
+	sessions: DataBase.config.vk.user.additional.map((tempUserToken) => {
 		return new VK({ token: tempUserToken });
 	}),
-	main: new VK({ token: config.vk.user.main }),
+	main: new VK({ token: DataBase.config.vk.user.main }),
 	getVK() {
 		return this.sessions[Math.floor(Math.random() * this.sessions.length)];
 	},
 };
 
 const group: IVKInstance = {
-	id: config.vk.group.id,
-	sessions: config.vk.group.additional.map((tempGroupToken) => {
+	id: DataBase.config.vk.group.id,
+	sessions: DataBase.config.vk.group.additional.map((tempGroupToken) => {
 		return new VK({ token: tempGroupToken });
 	}),
 	main: new VK({
-		pollingGroupId: config.vk.group.id,
-		token: config.vk.group.main,
+		pollingGroupId: DataBase.config.vk.group.id,
+		token: DataBase.config.vk.group.main,
 	}),
 	getVK() {
 		return this.sessions[Math.floor(Math.random() * this.sessions.length)];
