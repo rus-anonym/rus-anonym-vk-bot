4f1fe57244a79da2cfa1a5fc9a7d22be21a0ece3
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-12 08:50:32 +0300

    add save message to DB

--- src/lib/VK/middlewares/user/message.ts
+++ src/lib/VK/middlewares/user/message.ts
@@ -1,5 +1,18 @@
 import { user } from "../../core";
+import Models from "../../../DB/models";
 
 user.main.updates.on("message", async function (message) {
 	console.log(message);
+	const savedMessage = new Models.message({
+		id: message.id,
+		events: [message],
+		messageData: [
+			(
+				await user
+					.getVK()
+					.api.messages.getById({ message_ids: message.id, extended: 1 })
+			).items[0],
+		],
+	});
+	await savedMessage.save();
 });
