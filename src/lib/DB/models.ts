e5af5c90b88627960a8f7d4e320aa4c585afb563
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-14 06:30:43 +0300

    add models

--- src/lib/DB/models.ts
+++ src/lib/DB/models.ts
@@ -1,6 +1,8 @@
 import Schemes from "./schemes";
 import { typedModel } from "ts-mongoose";
 
+const user = typedModel("user", Schemes.user, "users");
+const chat = typedModel("chat", Schemes.chat, "chats");
 const message = typedModel("message", Schemes.message, "messages");
 
-export default { message };
+export default { user, chat, message };
