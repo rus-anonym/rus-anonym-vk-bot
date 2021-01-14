8a68048f0fa9b6122dd3679b87bde6aae9fb0aea
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-14 15:00:50 +0300

    update stats

--- src/commands/user/utils/stats.ts
+++ src/commands/user/utils/stats.ts
@@ -1,13 +1,18 @@
+import { ChatDocument } from "./../../../lib/DB/types";
 import ICommand from "../../../types/interfaces/commands";
 import DataBase from "../../../lib/DB/core";
 
 const command: ICommand = {
 	regexp: [/(?:^\.stats)$/i],
 	process: async function (message) {
+		const chatData: ChatDocument = await DataBase.models.chat.findOne({
+			peerId: message.peerId,
+		});
 		return await message.send(
-			`Messages in DB: ${await DataBase.models.message.countDocuments()}\nFrom this chat: ${
-				(await DataBase.models.message.find({ peerId: message.peerId })).length
-			}`,
+			`Messages in DB: ${await DataBase.models.message.countDocuments()}
+Users in DB: ${await DataBase.models.user.countDocuments()}
+Chats in DB: ${await DataBase.models.chat.countDocuments()}
+From this chat: ${chatData.messages.length}`,
 		);
 	},
 };
