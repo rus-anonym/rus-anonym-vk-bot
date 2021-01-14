9d4b1e68d425efd9bee42bdd03f7d3ef75444e4d
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-14 06:57:07 +0300

    add types

--- src/lib/DB/types.ts
+++ src/lib/DB/types.ts
@@ -1,4 +1,6 @@
 import { ExtractDoc } from "ts-mongoose";
 import Schemes from "./schemes";
 
+export type UserDocument = ExtractDoc<typeof Schemes.user>;
+export type ChatDocument = ExtractDoc<typeof Schemes.chat>;
 export type MessageDocument = ExtractDoc<typeof Schemes.message>;
