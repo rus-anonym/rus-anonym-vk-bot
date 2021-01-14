8448610ee0131cd17e4f258bd741c2018e89ef9f
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-14 15:31:28 +0300

    fix stats to personal dialogues

--- src/commands/user/utils/stats.ts
+++ src/commands/user/utils/stats.ts
@@ -1,18 +1,27 @@
-import { ChatDocument } from "./../../../lib/DB/types";
+import { UserDocument, ChatDocument } from "./../../../lib/DB/types";
 import ICommand from "../../../types/interfaces/commands";
 import DataBase from "../../../lib/DB/core";
 
 const command: ICommand = {
 	regexp: [/(?:^\.stats)$/i],
 	process: async function (message) {
-		const chatData: ChatDocument = await DataBase.models.chat.findOne({
-			id: message.chatId,
-		});
+		let fromThisChatMessages: number;
+		if (message.isChat) {
+			const chatData: ChatDocument = await DataBase.models.chat.findOne({
+				id: message.chatId,
+			});
+			fromThisChatMessages = chatData.messages.length;
+		} else {
+			const userData: UserDocument = await DataBase.models.user.find({
+				id: message.peerId,
+			});
+			fromThisChatMessages = userData.personalMessages.length;
+		}
 		return await message.send(
 			`Messages in DB: ${await DataBase.models.message.countDocuments()}
 Users in DB: ${await DataBase.models.user.countDocuments()}
 Chats in DB: ${await DataBase.models.chat.countDocuments()}
-From this chat: ${chatData.messages.length}`,
+From this chat: ${fromThisChatMessages}`,
 		);
 	},
 };
