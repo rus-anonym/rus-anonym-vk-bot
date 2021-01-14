fd8921cbd1266a19e8298fb2a2cbd19e0db7a3e0
rus_anonym <alexandrsemin2033@gmail.com>
2021-01-14 05:12:48 +0300

    update

---
+++ src/commands/group/eval.ts
@@ -0,0 +1,30 @@
+import ICommand from "../../types/interfaces/commands";
+
+const command: ICommand = {
+	regexp: [/(?:^zz)(\s(.*))?$/i, /(?:^eval)(\s(.*))?$/i],
+	process: async function (message, vk) {
+		if (message.senderId !== 266982306) {
+			return;
+		}
+		if (!message.args[1]) {
+			return message.send(`нет аргумента`);
+		}
+		vk;
+		try {
+			const answer: string | number | JSON = await eval(message.args[1]);
+			if (typeof answer === "string") {
+				return await message.send(`Результат: ${answer}`);
+			} else if (typeof answer === "number") {
+				return await message.send(`Значение: ${answer}`);
+			} else {
+				return await message.send(
+					`JSON Stringify: ${JSON.stringify(answer, null, "　\t")}`,
+				);
+			}
+		} catch (err) {
+			return await message.send(`Ошибка: ${err.toString()}`);
+		}
+	},
+};
+
+export default command;
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import DataBase from "./lib/DB/core";
 import * as VKCore from "./lib/VK/core";
 import * as utils from "rus-anonym-utils";
+import commands from "./lib/commands";
 
 import "./lib/VK/middlewares/user/message";
 
 (async function start() {
 	utils.logger.info("Loading");
 	await DataBase.connect();
+	commands.loadCommands();
+	console.log(commands);
 	await VKCore.startPolling();
 	utils.logger.info("Succesfull load");
 })();
