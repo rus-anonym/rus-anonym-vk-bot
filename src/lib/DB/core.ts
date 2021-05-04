721406f02636e580859c3e85b05cc989c06d2ba0
RusAnonym <alexandrsemin2033@gmail.com>
2021-05-05 02:52:09 +0300

    add mongodb connection

--- src/lib/DB/core.ts
+++ src/lib/DB/core.ts
@@ -1,7 +1,46 @@
-import config from "../../DB/config.json";
+import { typedModel } from "ts-mongoose";
+import mongoose from "mongoose";
 
+import config from "../../DB/config.json";
+import schemes from "./schemes";
 class DB {
 	public config = config;
+
+	public connection = mongoose.createConnection(
+		`mongodb+srv://${config.db.mongo.login}:${config.db.mongo.password}@${config.db.mongo.address}/${config.db.mongo.db}`,
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+		},
+	);
+
+	public models = {
+		message: typedModel(
+			"message",
+			schemes.message,
+			"messages",
+			undefined,
+			undefined,
+			this.connection,
+		),
+		user: typedModel(
+			"user",
+			schemes.user,
+			"users",
+			undefined,
+			undefined,
+			this.connection,
+		),
+		chat: typedModel(
+			"chat",
+			schemes.chat,
+			"chats",
+			undefined,
+			undefined,
+			this.connection,
+		),
+	};
 }
 
 export default new DB();
