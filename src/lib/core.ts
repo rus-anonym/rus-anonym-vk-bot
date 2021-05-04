ea37e2573adea4614873965abd5c75f636534d7d
RusAnonym <alexandrsemin2033@gmail.com>
2021-05-05 02:56:51 +0300

    update core

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,1 +1,7 @@
-import vk from "./VK/core";
+import VK from "./VK/core";
+import DB from "./DB/core";
+
+DB.connection.once("open", function MongoDBConnected() {
+	VK.user.main.updates.start();
+	VK.group.main.updates.start();
+});
