af51c0edbe7cc579a900a6bc6504aee55ad2c747
RusAnonym <alexandrsemin2033@gmail.com>
2021-05-05 03:39:05 +0300

    improve logger

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,7 +1,26 @@
+import moment from "moment";
+moment.locale("ru");
+
 import VK from "./VK/core";
 import DB from "./DB/core";
+import InternalUtils from "./utils/core";
 
 DB.connection.once("open", function MongoDBConnected() {
-	VK.user.main.updates.start();
-	VK.group.main.updates.start();
+	InternalUtils.logger.send(
+		`Script start at ${moment().format("HH:mm:ss.SSS | DD.MM.YYYY")}`,
+	);
+	VK.user.main.updates.start().then(() => {
+		InternalUtils.logger.send(
+			`VK User polling start at ${moment().format(
+				"HH:mm:ss.SSS | DD.MM.YYYY",
+			)}`,
+		);
+	});
+	VK.group.main.updates.start().then(() => {
+		InternalUtils.logger.send(
+			`VK Group polling start at ${moment().format(
+				"HH:mm:ss.SSS | DD.MM.YYYY",
+			)}`,
+		);
+	});
 });
--- src/lib/utils/lib/logger.ts
+++ src/lib/utils/lib/logger.ts
@@ -1,24 +1,45 @@
 import { getRandomId } from "vk-io";
+import os from "node-os-utils";
+import moment from "moment";
+
 import DB from "../../DB/core";
 import VK from "../../VK/core";
 
 type Log = "message" | "conversation" | "rest" | "error";
 export default class Logger {
-	public send(message: string, type: Log = "rest") {
+	public async send(message: string, type: Log = "rest") {
 		let selectedChat;
+		let prefix;
 
 		switch (type) {
 			case "message":
 				selectedChat = DB.config.vk.logs.conversations.messages;
+				prefix = "🆗";
+				break;
 			case "conversation":
 				selectedChat = DB.config.vk.logs.conversations.conversations;
+				prefix = "🆗";
+				break;
 			case "rest":
 				selectedChat = DB.config.vk.logs.conversations.rest;
+				prefix = "⚠";
+				break;
 			case "error":
 				selectedChat = DB.config.vk.logs.conversations.errors;
+				prefix = "⛔";
+				break;
 			default:
 				selectedChat = DB.config.vk.logs.conversations.errors;
+				prefix = "⛔";
+				break;
 		}
+
+		message = `Log: ${moment().format("HH:mm:ss.SSS | DD.MM.YYYY")}\n
+${prefix} - ${message}`;
+
+		message += "\n\n";
+
+		message += `🔴🔴🔴🔴🔴🔴🔴🔴`;
 
 		return VK.group.getVK().api.messages.send({
 			chat_id: selectedChat,
