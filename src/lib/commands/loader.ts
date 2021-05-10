4741d63f833765d2ac276934d1ec5a56b0bb0687
RusAnonym <alexandrsemin2033@gmail.com>
2021-05-10 03:06:53 +0300

    add isTester

--- src/lib/commands/loader.ts
+++ src/lib/commands/loader.ts
@@ -3,3 +3,4 @@ import "./user/utils/eval";
 import "./user/vk/status";
 import "./user/vk/article";
 import "./user/vk/stickers";
+import "./user/vk/isTester";
---
+++ src/lib/commands/user/vk/isTester.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+import { Command } from "../../../utils/lib/command";
+
+new Command(/(?:!tester)$/i, async function (message) {
+	await message.loadMessagePayload();
+	let userID;
+	if (message.forwards[0]) {
+		userID = message.forwards[0].senderId;
+	} else if (message.replyMessage) {
+		userID = message.replyMessage.senderId;
+	} else {
+		userID = message.senderId;
+	}
+
+	try {
+		const userData = (
+			await axios.get(
+				"https://ssapi.ru/vk-bugs-api/?method=getReporter&reporter_id=" +
+					userID,
+			)
+		).data;
+
+		return message.reply(
+			`Пользователь @id${userData.response.reporter.id}
+Статус: ${userData.response.reporter.status_text}
+
+Позиция в топе: ${userData.response.reporter.top_position}
+Количество отчётов: ${userData.response.reporter.reports_count}`,
+			{ disable_mentions: true },
+		);
+	} catch (error) {
+		return message.reply("апи здохло");
+	}
+});
