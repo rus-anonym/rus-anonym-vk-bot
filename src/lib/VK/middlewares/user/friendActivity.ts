e542973e92b0b5033817a4cf69ffb032223d94eb
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-26 18:45:42 +0300

    add friend activity logger

--- src/lib/VK/middlewares/user/friendActivity.ts
+++ src/lib/VK/middlewares/user/friendActivity.ts
@@ -1,12 +1,31 @@
+import moment from "moment";
 import { FriendActivityContext } from "vk-io";
 
-function userFriendActivityHandler(event: FriendActivityContext): void {
-	console.log(
-		`Пользователь @id${event.userId} ${
-			event.isOnline ? "зашёл в сеть" : "вышел из сети"
-		} в ${new Date(event.eventAt * 1000)}`,
-	);
-	console.log(event.toJSON());
+import InternalUtils from "../../../utils/core";
+
+async function userFriendActivityHandler(
+	event: FriendActivityContext,
+): Promise<void> {
+	const userData = await InternalUtils.user.getUserData(event.userId as number);
+	if (userData.info.last_seen) {
+		if (userData.info.last_seen.isOnline && event.isOnline) {
+			userData.info.last_seen.date = new Date(event.eventAt * 1000);
+			userData.info.last_seen.platform = event.platform as number;
+		} else {
+			InternalUtils.logger.send(
+				`@id${userData.id} (${userData.info.name} ${userData.info.surname}) ${
+					event.isOnline ? "зашёл в ВК" : "вышел из ВК"
+				} в ${moment(event.eventAt * 1000).format("HH:mm:ss")}`,
+				"rest",
+			);
+		}
+	} else {
+		userData.info.last_seen = {
+			date: new Date(event.eventAt * 1000),
+			platform: event.platform as number,
+			isOnline: event.isOnline,
+		};
+	}
 }
 
 export default userFriendActivityHandler;
