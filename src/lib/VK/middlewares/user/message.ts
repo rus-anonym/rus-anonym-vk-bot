37695f1a62b8d0720dbc6c622c927bafa6bb446f
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-26 17:25:23 +0300

    optimize??

--- src/lib/VK/middlewares/user/message.ts
+++ src/lib/VK/middlewares/user/message.ts
@@ -3,9 +3,7 @@ import { ModernMessageContext } from "./../../../utils/lib/command";
 import InternalUtils from "../../../utils/core";
 import VK from "../../../VK/core";
 
-async function userMessageHandler(
-	message: ModernMessageContext,
-): Promise<void> {
+function userMessageHandler(message: ModernMessageContext): void {
 	InternalUtils.user.saveMessage(message).catch((err) => {
 		InternalUtils.logger.send(
 			`Error on save message #${message.id}\n
@@ -26,7 +24,7 @@ https://vk.com/im?sel=${
 			message.args = selectedCommand.regexp.exec(
 				message.text,
 			) as RegExpExecArray;
-			await selectedCommand.process(message, TempVK).catch((err) => {
+			selectedCommand.process(message, TempVK).catch((err) => {
 				InternalUtils.logger.send(
 					`Error on execute command\nError: ${err.toString()}`,
 					"error",
