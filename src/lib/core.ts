d648cd8ff369c4fe109abb62b3207df28e2aea57
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-27 19:02:32 +0300

    add birthday logger

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,4 +1,6 @@
 import moment from "moment";
+import { Interval } from "simple-scheduler-task";
+
 moment.locale("ru");
 
 import VK from "./VK/core";
@@ -25,6 +27,25 @@ DB.connection.once("open", function MongoDBConnected() {
 			)}`,
 		);
 	});
+});
+
+new Interval({
+	source: async () => {
+		const users = await InternalUtils.user.getFriendsBirthday(new Date());
+		InternalUtils.logger.send(`
+Сегодня ${moment().format("DD.MM.YYYY")} день рождения празднуют:
+${users.map((user, index) => {
+	return `${index + 1}. @id${user.id}(${user.name} ${user.surname})`;
+})}`);
+	},
+	plannedTime: moment()
+		.add(1, "day")
+		.set("hour", 0)
+		.set("minute", 0)
+		.set("second", 0)
+		.toDate(),
+	type: "getBirthdays",
+	intervalTimer: 24 * 60 * 60 * 1000,
 });
 
 process.on("warning", async (warning) => {
