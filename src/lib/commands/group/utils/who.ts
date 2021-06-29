86b7b25be898c9ddcf2206c167779079b833d3da
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-30 01:07:34 +0300

    fix who command

--- src/lib/commands/group/utils/who.ts
+++ src/lib/commands/group/utils/who.ts
@@ -9,18 +9,25 @@ new GroupCommand(/(?:^кто\s)(.*)$/i, async function (message) {
 			message: `Работает только в чатах`,
 		});
 	}
+
+	if (message.args[1].endsWith("?")) {
+		message.args[1] = message.args[1].substring(0, message.args[1].length - 1);
+	}
+
 	const users = await VK.group.getVK().api.messages.getConversationMembers({
 		peer_id: message.peerId,
 	});
 
-	const randomUser = users.profiles?.find(
-		(x) =>
-			x.id ===
-			utils.array.random(users.items.filter((x) => x.member_id > 0)).member_id,
-	);
+	const randomUser = utils.array.random(
+		users.items.filter((x) => x.member_id > 0),
+	).member_id;
+
+	const [userData] = await VK.group
+		.getVK()
+		.api.users.get({ user_ids: randomUser.toString() });
 
 	return await message.sendMessage({
-		message: `Мне кажется что ${message.args[1]} это @id${randomUser?.id} (${randomUser?.first_name} ${randomUser?.last_name})`,
+		message: `Мне кажется что ${message.args[1]} это @id${userData?.id} (${userData?.first_name} ${userData?.last_name})`,
 		disable_mentions: true,
 	});
 });
