4a29643da3784da2abe8c47c21dbbaad6b439e84
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-29 21:03:11 +0300

    fix

--- src/lib/commands/user/utils/choose.ts
+++ src/lib/commands/user/utils/choose.ts
@@ -3,6 +3,6 @@ import { Command } from "../../../utils/lib/command";
 
 new Command(/(?:^!выбери\s)(.*)(?:\sили\s)(.*)$/i, async function (message) {
 	return await message.reply(
-		`Я выбираю ${message.args[utils.number.getRandomInt(0, 1) + 1]}`,
+		`Я выбираю ${message.args[utils.number.getRandomIntInclusive(0, 1) + 1]}`,
 	);
 });
