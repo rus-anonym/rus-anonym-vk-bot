f65d311c11df06005d6263d07f3f1748fcb52ddd
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-30 16:02:11 +0300

    optimize

--- src/lib/utils/core.ts
+++ src/lib/utils/core.ts
@@ -2,9 +2,11 @@ import { UserCommand, GroupCommand } from "./lib/commands";
 import UtilsLogger from "./lib/logger";
 import UtilsUser from "./lib/user";
 import UtilsGroup from "./lib/group";
+import UtilsCommands from "./lib/utilsCommands";
 
 class Utils {
 	public logger = new UtilsLogger();
+	public commands = new UtilsCommands();
 	public userCommands: UserCommand[] = [];
 	public groupCommands: GroupCommand[] = [];
 	public user = new UtilsUser();
