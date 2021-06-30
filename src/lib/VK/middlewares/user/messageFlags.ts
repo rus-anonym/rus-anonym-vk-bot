90c456941b7b94100ea740b5cac954c739298711
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-30 21:06:50 +0300

    update userMessageFlags handler

--- src/lib/VK/middlewares/user/messageFlags.ts
+++ src/lib/VK/middlewares/user/messageFlags.ts
@@ -1,13 +1,12 @@
-import { ContextDefaultState, MessageFlagsContext } from "vk-io";
+import { MessageFlagsContext } from "vk-io";
 
 import InternalUtils from "../../../utils/core";
 
-function userMessageFlags(
-	event: MessageFlagsContext<ContextDefaultState>,
-): void {
+function userMessageFlags(event: MessageFlagsContext, next: () => void): void {
 	if (event.isDeletedForAll) {
 		InternalUtils.user.processDeletedMessage(event);
 	}
+	next();
 }
 
 export default userMessageFlags;
