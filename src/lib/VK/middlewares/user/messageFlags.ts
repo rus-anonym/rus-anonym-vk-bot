0c486f1866f0664f36c8a86396e7471da6df799e
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-01 00:03:49 +0300

    fix message_flags handler

--- src/lib/VK/middlewares/user/messageFlags.ts
+++ src/lib/VK/middlewares/user/messageFlags.ts
@@ -5,6 +5,7 @@ import InternalUtils from "../../../utils/core";
 function userMessageFlags(event: MessageFlagsContext, next: () => void): void {
 	if (event.isDeletedForAll) {
 		InternalUtils.user.processDeletedMessage(event);
+		return;
 	}
 	next();
 }
