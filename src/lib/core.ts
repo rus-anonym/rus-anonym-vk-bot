8802d63b68686387b96fdd5e31f0771518d89527
RusAnonym <alexandrsemin2033@gmail.com>
2021-06-30 15:00:36 +0300

    intervals to separate file

--- src/lib/core.ts
+++ src/lib/core.ts
@@ -1,5 +1,4 @@
 import moment from "moment";
-import { Interval } from "simple-scheduler-task";
 
 moment.locale("ru");
 
@@ -7,6 +6,7 @@ import VK from "./VK/core";
 import DB from "./DB/core";
 import InternalUtils from "./utils/core";
 
+import "./intervals";
 import "./commands/loader";
 
 DB.user.connection.once("open", () => {
@@ -33,27 +33,6 @@ DB.group.connection.once("open", () => {
 			)}`,
 		);
 	});
-});
-
-new Interval({
-	source: async () => {
-		const users = await InternalUtils.user.getFriendsBirthday(new Date());
-		InternalUtils.logger.send(
-			`Сегодня ${moment().format("DD.MM.YYYY")} день рождения празднуют:
-${users.map((user, index) => {
-	return `${index + 1}. @id${user.id}(${user.name} ${user.surname})`;
-})}`,
-			"info",
-		);
-	},
-	plannedTime: moment()
-		.add(1, "day")
-		.set("hour", 0)
-		.set("minute", 0)
-		.set("second", 0)
-		.toDate(),
-	type: "getBirthdays",
-	intervalTimer: 24 * 60 * 60 * 1000,
 });
 
 process.on("warning", async (warning) => {
