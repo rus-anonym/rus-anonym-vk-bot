43bc95df30904ec60f8dd4854d6b3d07f13c5d45
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-06 13:51:18 +0300

    add about command

--- src/lib/commands/user/utils/about.ts
+++ src/lib/commands/user/utils/about.ts
@@ -1,11 +1,17 @@
 import { UserCommand } from "../../../utils/lib/commands";
 import DB from "../../../DB/core";
 
-new UserCommand(/(?:^!about|!статус)$/i, async function (message) {
+new UserCommand(/(?:^!about)$/i, async function (message) {
 	return message.editMessage({
-		message: `DB Stats:
+		message: `User DB Stats:
 Messages: ${await DB.user.models.message.countDocuments()}
 Users: ${await DB.user.models.user.countDocuments()}
-Chats: ${await DB.user.models.chat.countDocuments()}`,
+Chats: ${await DB.user.models.chat.countDocuments()}
+
+Group DB Stats:
+Users: ${await DB.group.models.user.countDocuments()}
+
+Author: @rus_anonym
+Source Code: https://github.com/RusAnonym/rus-anonym-vk-bot`,
 	});
 });
