efe1b435e96541f6d6f55b6b522b462ef6686040
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-16 16:40:35 +0300

    add !sync command???

---
+++ src/lib/commands/user/utils/sync.ts
@@ -0,0 +1,27 @@
+// import { UserCommand } from "../../../utils/lib/commands";
+
+// import DB from "../../../DB/core";
+
+// new UserCommand(/(?:^!sync)$/i, async function (message) {
+// 	let log = `User DB Log:\n`;
+// 	const userDBUsers = await DB.user.models.user.updateMany(
+// 		{
+// 			"info.full": {
+// 				$exists: true,
+// 			},
+// 			"info.full.settings": {
+// 				$exists: false,
+// 			},
+// 		},
+// 		{
+// 			"info.full.settings": {},
+// 			"info.full.settings.getAudios": true,
+// 		},
+// 	);
+// 	log += `Users updated:
+// Matched: ${userDBUsers.n}
+// Modified: ${userDBUsers.nModified}\n`;
+// 	await message.editMessage({
+// 		message: log,
+// 	});
+// });
