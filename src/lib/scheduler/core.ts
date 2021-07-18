56ea3502b340b60b7d8e3047ac3a560b25f8c31c
RusAnonym <alexandrsemin2033@gmail.com>
2021-07-18 05:01:54 +0300

    add import for createBalabolaPost

--- src/lib/scheduler/core.ts
+++ src/lib/scheduler/core.ts
@@ -3,6 +3,7 @@ import cleanOldMessages from "./tasks/cleanOldMessages";
 import getBirthdays from "./tasks/getBirthdays";
 import updateOnlinePrivacySettings from "./tasks/updateOnlinePrivacySettings";
 import deleteSameAudios from "./tasks/deleteSameAudios";
+import createBalabolaPost from "./tasks/createBalabolaPost";
 
 export default {
 	updateUserData,
@@ -10,4 +11,5 @@ export default {
 	getBirthdays,
 	deleteSameAudios,
 	updateOnlinePrivacySettings,
+	createBalabolaPost,
 };
