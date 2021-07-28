48b2900d3a14de94260a3ed7539296d4fdc095bd
rus_anonym <alexandrsemin2033@gmail.com>
2021-07-28 21:52:39 +0300

    update GroupCommand class

--- src/lib/utils/lib/commands/core.ts
+++ src/lib/utils/lib/commands/core.ts
@@ -50,6 +50,8 @@ export class UserCommand extends Command {
 
 export class GroupCommand extends Command {
 	public isSelf: boolean;
+	public type: "callback" | "regexp";
+	public callbackTrigger: string;
 
 	public process: (
 		message: MessageContext<GroupModernMessageContextState>,
@@ -60,6 +62,8 @@ export class GroupCommand extends Command {
 		regexp,
 		process,
 		isSelf = false,
+		type = "regexp",
+		callbackTrigger = "null",
 	}: {
 		regexp: RegExp;
 		process: (
@@ -67,11 +71,23 @@ export class GroupCommand extends Command {
 			vk: VK,
 		) => Promise<unknown>;
 		isSelf?: boolean;
+		type?: "callback" | "regexp";
+		callbackTrigger?: string;
 	}) {
 		super(regexp);
 		this.isSelf = isSelf;
+		this.type = type;
+		this.callbackTrigger = callbackTrigger;
 		this.process = process;
 		InternalUtils.groupCommands.addCommand(this);
+	}
+
+	public check(input: string): boolean {
+		if (this.type === "callback") {
+			return this.callbackTrigger === input;
+		} else {
+			return this.regexp.test(input);
+		}
 	}
 }
 
