467c4d470db464d0e9e9d192a42cb4b0fdff8a8c
rus_anonym <alexandrsemin2033@gmail.com>
2021-07-28 20:12:23 +0300

    improve tiktok group command

--- src/lib/commands/group/utils/tiktok.ts
+++ src/lib/commands/group/utils/tiktok.ts
@@ -3,22 +3,34 @@ import axios from "axios";
 import { GroupCommand } from "../../../utils/lib/commands/core";
 
 new GroupCommand({
-		regexp: /(?:^\/tiktok)(\s(.*))$/i, process: async function (message) {
+	regexp: /(?:^\/tiktok)(\s(.*))$/i,
+	process: async function (context) {
+		await context.state.sendMessage({
+			message: `Обрабатываю видео...`,
+		});
+
+		try {
 			const response = await (
 				await axios({
 					url: "https://godownloader.com/api/tiktok-no-watermark-free",
 					params: {
-						url: message.state.args[1],
+						url: context.state.args[1],
 						key: "godownloader.com",
 					},
 				})
 			).data;
 
-			return await message.state.sendMessage({
+			return await context.state.sendMessage({
 				message: `TikTok video:
 Video URL: ${response.video_no_watermark}
 Music URL: ${response.music_url}`,
 				dont_parse_links: true,
 			});
+		} catch (error) {
+			return await context.state.sendMessage({
+				message: `Ошибка: ${error.toString()}`,
+				dont_parse_links: true,
+			});
 		}
-	});
+	},
+});
