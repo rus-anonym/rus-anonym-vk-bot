42f0e68e7680242735277b8b63e13eaeca4b57ff
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-01 10:51:34 +0300

    comment all tasks

--- src/lib/scheduler/core.ts
+++ src/lib/scheduler/core.ts
@@ -1,30 +1,30 @@
-import scheduler from "simple-scheduler-task";
+// import scheduler from "simple-scheduler-task";
 
-import InternalUtils from "../utils/core";
+// import InternalUtils from "../utils/core";
 
-// import updateUserData from "./tasks/updateUsersData";
-import cleanOldMessages from "./tasks/cleanOldMessages";
-import getBirthdays from "./tasks/getBirthdays";
-import updateOnlinePrivacySettings from "./tasks/updateOnlinePrivacySettings";
-import deleteSameAudios from "./tasks/deleteSameAudios";
-import deleteSameDocuments from "./tasks/deleteSameDocuments";
-import sendHappyBirthdayGreetings from "./tasks/sendHappyBirthdayGreetings";
+// // import updateUserData from "./tasks/updateUsersData";
+// import cleanOldMessages from "./tasks/cleanOldMessages";
+// import getBirthdays from "./tasks/getBirthdays";
+// import updateOnlinePrivacySettings from "./tasks/updateOnlinePrivacySettings";
+// import deleteSameAudios from "./tasks/deleteSameAudios";
+// import deleteSameDocuments from "./tasks/deleteSameDocuments";
+// import sendHappyBirthdayGreetings from "./tasks/sendHappyBirthdayGreetings";
 
-scheduler.events.on("error", (error) => {
-	InternalUtils.logger.send({
-		message: `Ошибка при выполнении запланированной задачи:
-Тип: ${error.task.type}
-Error: ${error.error.toString()}`,
-		type: "error",
-	});
-});
+// scheduler.events.on("error", (error) => {
+// 	InternalUtils.logger.send({
+// 		message: `Ошибка при выполнении запланированной задачи:
+// Тип: ${error.task.type}
+// Error: ${error.error.toString()}`,
+// 		type: "error",
+// 	});
+// });
 
-export default {
-	// updateUserData,
-	cleanOldMessages,
-	getBirthdays,
-	deleteSameAudios,
-	updateOnlinePrivacySettings,
-	sendHappyBirthdayGreetings,
-	deleteSameDocuments,
-};
+// export default {
+// 	// updateUserData,
+// 	cleanOldMessages,
+// 	getBirthdays,
+// 	deleteSameAudios,
+// 	updateOnlinePrivacySettings,
+// 	sendHappyBirthdayGreetings,
+// 	deleteSameDocuments,
+// };
