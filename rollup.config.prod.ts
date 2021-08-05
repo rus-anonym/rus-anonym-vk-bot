5f014cacc5260f7e235639cb7fee113cde3accc3
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-06 02:34:55 +0300

    add rollup build

---
+++ rollup.config.prod.ts
@@ -0,0 +1,34 @@
+import fs from "fs";
+import * as path from "path";
+
+import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import { uglify } from "rollup-plugin-uglify";
+
+const distributionPath = path.resolve(__dirname, "dist");
+
+if (process.version >= "v14.4") {
+	fs.rmSync(distributionPath, {
+		recursive: true,
+		force: true,
+	});
+} else {
+	fs.rmdirSync(distributionPath, {
+		recursive: true,
+	});
+}
+
+export default {
+	input: "./src/main.ts",
+	output: {
+		file: "./dist/bundle.js",
+		format: "cjs",
+	},
+	plugins: [
+		json(),
+		typescript({ tsconfig: "./tsconfig.json" }),
+		commonjs({ extensions: [".js", ".ts"] }),
+		uglify()
+	],
+};
