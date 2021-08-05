884d53c214527c300607f82b729d58d700d71b94
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-06 00:38:01 +0300

    repeat default 1

--- src/lib/commands/user/utils/repeat.ts
+++ src/lib/commands/user/utils/repeat.ts
@@ -6,9 +6,9 @@ import { UserCommand } from "../../../utils/lib/commands/core";
 new UserCommand(/(?:^!repeat)(?:\s(\d+))?(\strue)?$/i, async function (
 	message,
 ) {
-	message.state.args[1] = message.state.args[1] || "3";
+	message.state.args[1] = message.state.args[1] || "1";
 	if (!Number(message.state.args[1])) {
-		message.state.args[1] = "3";
+		message.state.args[1] = "1";
 	}
 	if (Number(message.state.args[1]) > 100) {
 		return await message.editMessage({
