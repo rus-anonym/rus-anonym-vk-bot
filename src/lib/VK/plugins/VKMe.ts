62a443dcddf20e0536ef2e2dc3f61c7bbc2415b7
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-06 21:03:38 +0300

    change VKMe authorization type

--- src/lib/VK/plugins/VKMe.ts
+++ src/lib/VK/plugins/VKMe.ts
@@ -9,7 +9,7 @@ class VKMe extends Authorization {
 			app_id: officialAppCredentials.vkMe.clientId,
 			scope: "0",
 			secret: officialAppCredentials.vkMe.clientSecret,
-			type: "DirectAuthorization",
+			type: "ImplicitFlowUser",
 			apiVersion: DB.constants.vk.user.defaultParams.apiVersion,
 			apiOptions: {
 				...DB.constants.vk.user.defaultParams,
