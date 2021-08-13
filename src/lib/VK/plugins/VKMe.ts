f916aa4b6f36c679ca9b378273d86e9eec535ecb
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-13 15:17:21 +0300

    disable vk me

--- src/lib/VK/plugins/VKMe.ts
+++ src/lib/VK/plugins/VKMe.ts
@@ -1,21 +1,21 @@
-import { officialAppCredentials } from "@vk-io/authorization";
+// import { officialAppCredentials } from "@vk-io/authorization";
 
-import { Authorization } from "./authorization";
-import DB from "../../DB/core";
+// import { Authorization } from "./authorization";
+// import DB from "../../DB/core";
 
-class VKMe extends Authorization {
-	constructor() {
-		super({
-			app_id: officialAppCredentials.vkMe.clientId,
-			scope: "0",
-			secret: officialAppCredentials.vkMe.clientSecret,
-			type: "ImplicitFlowUser",
-			apiVersion: DB.constants.vk.user.defaultParams.apiVersion,
-			apiOptions: {
-				...DB.constants.vk.user.defaultParams,
-			},
-		});
-	}
-}
+// class VKMe extends Authorization {
+// 	constructor() {
+// 		super({
+// 			app_id: officialAppCredentials.vkMe.clientId,
+// 			scope: "0",
+// 			secret: officialAppCredentials.vkMe.clientSecret,
+// 			type: "ImplicitFlowUser",
+// 			apiVersion: DB.constants.vk.user.defaultParams.apiVersion,
+// 			apiOptions: {
+// 				...DB.constants.vk.user.defaultParams,
+// 			},
+// 		});
+// 	}
+// }
 
-export default VKMe;
+// export default VKMe;
