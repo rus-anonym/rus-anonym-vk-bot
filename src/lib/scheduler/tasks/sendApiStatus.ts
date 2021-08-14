e05cf528e890d3a6e136a9995680cece1b1cb5ce
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-15 01:45:06 +0300

    improve api logs

--- src/lib/scheduler/tasks/sendApiStatus.ts
+++ src/lib/scheduler/tasks/sendApiStatus.ts
@@ -1,21 +1,50 @@
 import moment from "moment";
 import utils from "rus-anonym-utils";
+import types from "rus-anonym-utils/dist/lib/VK/types";
 import { Interval } from "simple-scheduler-task";
 import { getRandomId } from "vk-io";
 
 import DB from "../../DB/core";
 import VK from "../../VK/core";
 
+let VK_API_STATUS: types.IVKAPIStatus[] | null = null;
+
 async function sendApiStatus() {
-	const VK_API_STATUS = await utils.vk.api.status();
-	await VK.group.getAPI().messages.send({
-		random_id: getRandomId(),
-		chat_id: DB.config.VK.group.logs.conversations.api,
-		message: `Состояние API VK на ${moment().format("HH:mm:ss, DD.MM.YYYY")}
-\n${VK_API_STATUS.map((section) => {
-			return `${section.section} - ${section.performance}ms (uptime: ${section.uptime}%)\n`;
-		}).join("")}`,
-	});
+	const NEW_VK_API_STATUS = await utils.vk.api.status();
+	if (
+		VK_API_STATUS &&
+		JSON.stringify(NEW_VK_API_STATUS) !== JSON.stringify(VK_API_STATUS)
+	) {
+		let message = "";
+		for (const section of NEW_VK_API_STATUS) {
+			const oldSectionData = VK_API_STATUS.find(
+				(x) => x.section === section.section,
+			);
+
+			if (oldSectionData) {
+				message += `${section.section} [${section.performance}ms] ${
+					section.performance === oldSectionData.performance
+						? ""
+						: section.performance < oldSectionData.performance
+						? `(${section.performance - oldSectionData.performance}ms)`
+						: `(+${section.performance - oldSectionData.performance}ms)`
+				} - ${section.uptime}% ${
+					section.uptime === oldSectionData.uptime
+						? ""
+						: section.uptime < oldSectionData.uptime
+						? `(${section.uptime - oldSectionData.uptime}%)`
+						: `(+${section.uptime - oldSectionData.uptime}%)`
+				}\n`;
+			}
+		}
+		await VK.group.getAPI().messages.send({
+			random_id: getRandomId(),
+			chat_id: DB.config.VK.group.logs.conversations.api,
+			message: `Состояние API VK на ${moment().format("HH:mm:ss, DD.MM.YYYY")}
+\n${message}`,
+		});
+	}
+	VK_API_STATUS = NEW_VK_API_STATUS;
 }
 
 export default new Interval({
