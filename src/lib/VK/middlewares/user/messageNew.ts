4c99f780b6a562a758069be8b3c623fa2a4a9f36
rus_anonym <alexandrsemin2033@gmail.com>
2021-08-28 01:34:13 +0300

    small improves

--- src/lib/VK/middlewares/user/messageNew.ts
+++ src/lib/VK/middlewares/user/messageNew.ts
@@ -22,10 +22,21 @@ async function userMessageNew(
 			message.state.args = selectedCommand.regexp.exec(
 				message.text,
 			) as RegExpExecArray;
+			const sendMessage = message.send.bind(message);
+			message.send = (text, params = {}) => {
+				if (typeof text === "string") {
+					return sendMessage({
+						message: text + "&#13;",
+						...params,
+					});
+				} else {
+					text.message ? (text.message += "&#13;") : null;
+					return sendMessage(text);
+				}
+			};
 			selectedCommand.process(message, TempVK).catch((err) => {
 				InternalUtils.logger.send({
-					message: `Error on execute command\nError: ${err.toString()}
-JSON Stringify: ${JSON.stringify(err.toJSON(), null, "\t")}`,
+					message: `Error on execute command\nError: ${err.toString()}`,
 					type: "error",
 				});
 			});
