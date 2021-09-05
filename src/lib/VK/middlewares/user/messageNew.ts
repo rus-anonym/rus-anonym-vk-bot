78590eddd1a453f183a4d650e808109f2dec8118
rus_anonym <alexandrsemin2033@gmail.com>
2021-09-05 23:08:54 +0300

    old text improver

--- src/lib/VK/middlewares/user/messageNew.ts
+++ src/lib/VK/middlewares/user/messageNew.ts
@@ -1,3 +1,5 @@
+import utils from "rus-anonym-utils";
+
 import { MessageContext } from "vk-io";
 import { UserModernMessageContextState } from "../../../utils/lib/commands/core";
 
@@ -5,50 +7,18 @@ import InternalUtils from "../../../utils/core";
 import VK from "../../core";
 
 const improveText = (text: string) => {
-	if (/^(?:\[club\d+\|.*])/.test(text)) {
+	if (text.includes("&#13;") || /^(?:\[club\d+\|.*])/.test(text)) {
 		return text;
 	}
-	const isWord = (string: string) => /(?=[^\d])[\wа-яё]/i.test(string);
-	const isUpperCase = (string: string) => /[А-ЯЁA-Z]/.test(string);
-	const splittedText = text.split("");
-	const outArray = [];
-	let isNewLine = true;
-	for (const stringIndex in splittedText) {
-		const index = Number(stringIndex);
-		const currentSymbol = splittedText[index];
-		if (isNewLine) {
-			outArray.push(currentSymbol.toUpperCase());
-			isNewLine = false;
-			continue;
-		}
-		splittedText.reverse();
-		const prevSymbol = splittedText.find(
-			(element, elementIndex) =>
-				element !== " " && elementIndex > splittedText.length - (index + 1),
-		);
-		splittedText.reverse();
-		if (currentSymbol === "\n") {
-			if (prevSymbol && isWord(prevSymbol)) {
-				outArray.push(".");
-			}
-			outArray.push(currentSymbol);
-			isNewLine = true;
-			continue;
-		}
-		outArray.push(currentSymbol);
-		const nextSymbol = splittedText.find(
-			(element, elementIndex) => element !== " " && elementIndex > index,
-		);
-		if (nextSymbol && isWord(currentSymbol)) {
-			if (isUpperCase(nextSymbol)) {
-				outArray.push(".");
-			}
-		}
-		if (index === splittedText.length - 1 && isWord(currentSymbol)) {
-			outArray.push(".");
-		}
+	let newText = text;
+	if (/(?=[^\d])[\wа-яё]/i.test(utils.array.last(text.split("")))) {
+		newText += ".";
 	}
-	return outArray.join("");
+	if (/(?=[^\d])[\wа-яё]/.test(text[0])) {
+		newText = newText.substring(1);
+		newText = text[0].toUpperCase() + newText;
+	}
+	return newText;
 };
 
 async function userMessageNew(
