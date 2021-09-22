c192c6abfdd9d1b110e8274ef7b66d4fc232c959
rus_anonym <alexandrsemin2033@gmail.com>
2021-09-22 21:00:35 +0300

    improve slave command constructor

--- src/lib/commands/slave/choose.ts
+++ src/lib/commands/slave/choose.ts
@@ -1,12 +1,10 @@
 import utils from "rus-anonym-utils";
 import { SlaveCommand } from "../../utils/lib/commands/core";
 
-new SlaveCommand(/(?:^раб выбери\s)(.*)(?:\sили\s)(.*)$/i, async function (
-	message,
-) {
-	return await message.reply(
-		`Я выбираю ${
-			message.state.args[utils.number.getRandomIntInclusive(0, 1) + 1]
-		}`,
-	);
-});
+new SlaveCommand({
+		regexp: /(?:^раб выбери\s)(.*)(?:\sили\s)(.*)$/i, process: async function (
+			message) {
+			return await message.reply(
+				`Я выбираю ${message.state.args[utils.number.getRandomIntInclusive(0, 1) + 1]}`);
+		}
+	});
