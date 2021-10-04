d32bdf2a369194aee1426d5f457bbe5c55af36ed
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-05 01:28:56 +0300

    add vk conversation scheme

--- src/lib/DB/core.ts
+++ src/lib/DB/core.ts
@@ -121,6 +121,14 @@ class MainDB extends DB {
 			undefined,
 			this.connection,
 		),
+		vkConversation: typedModel(
+			"conversation",
+			mainSchemes.conversation,
+			"vk-conversation",
+			undefined,
+			undefined,
+			this.connection,
+		),
 	};
 
 	public schemes = mainSchemes;
