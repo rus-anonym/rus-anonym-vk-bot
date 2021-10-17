c381ffc2733cfcbc0bf034ed1b31abd07f78b6d8
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-18 01:03:26 +0300

    add mailing method

--- bot/src/lib/utils/lib/group.ts
+++ bot/src/lib/utils/lib/group.ts
@@ -1,5 +1,8 @@
+import utils from "rus-anonym-utils";
+
 import { ExtractDoc } from "ts-mongoose";
 import { getRandomId } from "vk-io";
+import { MessagesSendParams } from "vk-io/lib/api/schemas/params";
 
 import VK from "../../VK/core";
 import DB from "../../DB/core";
@@ -34,5 +37,37 @@ export default class UtilsGroup {
 			return newUserData;
 		}
 		return userData;
+	}
+
+	public async mailing(options: MessagesSendParams) {
+		for (const subGroup of VK.subGroups) {
+			const mailingUsers = (await DB.group.models.user
+				.find({
+					regGroupId: subGroup.id,
+					isMailingAllowed: true,
+				})
+				.distinct("id")) as number[];
+
+			for (const peer_ids of utils.array.splitTo(mailingUsers, 100)) {
+				const response = (await subGroup.getAPI().messages.send({
+					...options,
+					peer_ids,
+					random_id: getRandomId(),
+				})) as unknown as Array<{
+					peer_id: number;
+					error?: unknown;
+				}>;
+				for (const userResponse of response) {
+					if (userResponse.error) {
+						await DB.group.models.user.updateOne(
+							{
+								id: userResponse.peer_id,
+							},
+							{ isMailingAllowed: false },
+						);
+					}
+				}
+			}
+		}
 	}
 }
