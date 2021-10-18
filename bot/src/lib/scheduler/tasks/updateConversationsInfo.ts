a51682c716a2be5d8d10dd73bea2a4682af7846d
rus_anonym <alexandrsemin2033@gmail.com>
2021-10-19 02:31:36 +0300

    updateConversationsInfo every 5 minutes

--- bot/src/lib/scheduler/tasks/updateConversationsInfo.ts
+++ bot/src/lib/scheduler/tasks/updateConversationsInfo.ts
@@ -1,5 +1,6 @@
 import { Interval } from "simple-scheduler-task";
 import { getRandomId } from "vk-io";
+import moment from "moment";
 
 import VK from "../../VK/core";
 import DB from "../../DB/core";
@@ -7,12 +8,27 @@ import DB from "../../DB/core";
 async function updateConversationsInfo(): Promise<{
 	update: number;
 	delete: number;
+	outdated: number;
+	total: number;
 }> {
 	const response = {
 		update: 0,
 		delete: 0,
+		outdated: 0,
+		total: 0,
 	};
-	for await (const conversation of DB.main.models.vkConversation.find()) {
+
+	const minimalUpdateDate = moment().subtract(1, "day").toDate();
+	response.total = await DB.main.models.vkConversation.count();
+	const perUpdate = response.total / (24 * 12) + 5;
+
+	for await (const conversation of DB.main.models.vkConversation
+		.find({
+			updateDate: {
+				$lt: minimalUpdateDate,
+			},
+		})
+		.limit(perUpdate)) {
 		try {
 			const conversationInfo = await VK.fakes
 				.getUserFakeAPI()
@@ -23,13 +39,20 @@ async function updateConversationsInfo(): Promise<{
 			conversation.members = conversationInfo.preview.members;
 			conversation.updateDate = new Date();
 			conversation.markModified("members");
-			response.update++;
+			++response.update;
 			await conversation.save();
 		} catch {
-			response.delete++;
+			++response.delete;
 			await conversation.delete();
 		}
 	}
+
+	response.outdated = await DB.main.models.vkConversation.count({
+		updateDate: {
+			$lt: minimalUpdateDate,
+		},
+	});
+
 	return response;
 }
 
@@ -37,14 +60,16 @@ export default new Interval({
 	isInform: true,
 	type: "updateConversationsInfo",
 	source: updateConversationsInfo,
-	cron: "0 */12 * * *",
+	cron: "*/5 * * * *",
+	plannedTime: Date.now(),
 	onDone: (log) => {
 		VK.group.getAPI().messages.send({
 			random_id: getRandomId(),
 			chat_id: DB.config.VK.group.logs.conversations.conversationsTrack,
 			message: `Данные о беседах обновлены
 Обновлено: ${(log.response as any).update}
-Удалено: ${(log.response as any).delete}`,
+Удалено: ${(log.response as any).delete}
+Устаревших: ${(log.response as any).outdated}`,
 		});
 	},
 });
